29a7f45c91df20ba5bfd6a7397f3b9a433c86166
HyeongBeenYeom <121592971+HyeongBeenYeom@users.noreply.github.com>
2024-03-23 08:49:45 +0900

    [level 2] Title: H-Index, Time: 0.01 ms, Memory: 3.71 MB -BaekjoonHub

---
+++ 프로그래머스/2/42747. H－Index/H－Index.cpp
@@ -0,0 +1,20 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <iostream>
+
+using namespace std;
+
+int solution(vector<int> citations) {
+    int answer = 0;
+    sort(citations.begin(), citations.end(), greater<int>());
+    if(citations[0] == 0)
+        return 0;
+    for(int i=0;i<citations.size();i++){
+        if(citations[i] >= i+1)
+            answer = i+1;
+        else
+            break;
+    }
+    return answer;
+}
